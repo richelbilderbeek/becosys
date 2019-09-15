64f5adc77e32cfbc044f7a7ba9eabdd9116756ae
richelbilderbeek <richel@richelbilderbeek.nl>
2019-09-15 13:14:19 +0200

    Fix 'check_conditioned_on'

--- R/check_conditioned_on.R
+++ R/check_conditioned_on.R
@@ -4,6 +4,9 @@
 #' @inheritParams default_params_doc
 #' @export
 check_conditioned_on <- function(conditioned_on) {
+  if (length(conditioned_on) != 1) {
+    stop("'conditioned_on' must be one string")
+  }
   if (!conditioned_on %in% c("nothing", "non_extinction")) {
     stop("'conditioned_on' must be either 'nothing' or 'non_extinction'")
   }
